700d1e2b432482d19107d25ff74b5289b09e705b
kmerillat8216 <merillatk@huntington.edu>
2025-11-20 13:10:05 -0500

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -2,10 +2,10 @@ sql
 
 CREATE TABLE player (
 	player_id integer auto_increment PRIMARY KEY,
-	name varChar,
-  	position_id varChar FOREIGN KEY,
- 	year varChar,
-  	height varChar,
+	name varChar(100),
+  	position_id integer FOREIGN KEY,
+ 	year varChar(20),
+  	height varChar(10),
  	weight integer,
   	number integer
 )ENGINE=InnoDB;
@@ -16,8 +16,21 @@ CREATE TABLE game (
 	result ENUM('W', 'L'),
 	home_score integer,
 	away_score integer,
-	opponent varChar,
-	location varChar
+	opponent varChar(100),
+	location varChar(100)
 )ENGINE=InnoDB;
 
+CREATE TABLE position (
+	position_id integer PRIMARY KEY,
+	description varChar(50)
+)ENGINE=InnoDB;
 
+CREATE TABLE player_game_stats(
+	player_id integer,
+	game_id integer,
+	rushing_yards integer,
+	receiving_yards integer,
+	passing_yards integer,
+	touchdowns integer,
+	played boolean
+)ENGINE=InnoDB;
